863c952e42194d55b42ba800f89dbc684d18241e
maylexalayola-commits <maylexalayola@gmail.com>
2026-02-14 10:43:54 +0800

    m

--- valentines website/js/app.js
+++ valentines website/js/app.js
@@ -24,21 +24,21 @@ gifs.forEach(gifSrc => {
 no.addEventListener("click", () => {
   if (count == 2) {
     gif.src = "../resources/rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
-    text.innerHTML = "You meant to press YES right?🤨";
+    text.innerHTML = "wrong click ka beh?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
     gif.src = "../resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
-    text.innerHTML = "Your hand must have slipped right?🥹";
+    text.innerHTML = "ayaw mo talaga?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
     gif.src = "../resources/4.crying.gif";// gif credit: https://tenor.com/view/cat-kitty-gif-25340141
-    text.innerHTML = "I'm gonna cry😭";
+    text.innerHTML = "sakit mo naman ya😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
     no.style.fontSize = "4vh";
@@ -46,7 +46,7 @@ no.addEventListener("click", () => {
     count++;
   } else if (count == 5) {
     gif.src = "../resources/5.crying.gif";// gif credit: https://tenor.com/view/cat-gif-10173437195524493032
-    text.innerHTML = "Pretty Please🥺😘";
+    text.innerHTML = "sigi na bai🥺";
     yes.style.height = "90%";
     yes.style.width = "96%";
     no.style.display = "none";
@@ -57,8 +57,8 @@ no.addEventListener("click", () => {
 yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
-  text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/loop.codes/">Message me</a>';// inside the " " put your social media profile link
+  text.innerHTML = "yeyyyy thanks krus 😘";
+  yes.innerHTML = '<a href="https://www.instagram.com/axellemfaok?igsh=MXZ0M3JoNGoyMmRsaQ==">Message me</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
